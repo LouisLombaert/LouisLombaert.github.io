1417f16870d190af9c2613f859b456ed31945b3f
LouisLombaert <71373079+LouisLombaert@users.noreply.github.com>
2020-12-06 18:32:01 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-//index 
+let tableauPersonnage = [];
+let tableauEquipement = [];
+function recupererPersonnage(formulaire){
+	tableauPersonnage = [formulaire.nom.value, formulaire.sexe.value, formulaire.race.value, formulaire.classe.value];
+	tableauEquipement = [formulaire.arme.value];
+	return false ;
+};
